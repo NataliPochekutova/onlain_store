745f2c5b5ba07f93935b62ecf5eddca54001d3ed
Natalia Pochekutova <natalipochekutova51@gmail.com>
2025-07-22 23:29:50 +0800

    homework_27_v.1

---
+++ users/forms.py
@@ -0,0 +1,18 @@
+from users.models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from catalog.forms import StyleFormMixin
+
+
+class UserRegisterForm(StyleFormMixin, UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ("email", "password1", "password2")
+
+
+#class UserEditForm(StyleFormMixin, UserChangeForm):
+#    username = None
+
+#    class Meta:
+#        model = User
+#        fields = ("email", "country", "avatar", "phone")
